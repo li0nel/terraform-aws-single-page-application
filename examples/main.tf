a5da7ff93b80c21760994bc34c3d6d2c0af2a8d8
Lionel Martin <corsaaaire@gmail.com>
2018-06-01 11:21:52 +0100

    bump version

--- examples/main.tf
+++ examples/main.tf
@@ -23,7 +23,7 @@ provider "aws" {
 
 module "single-page-application" {
   source  = "li0nel/single-page-application/aws"
-  version = "0.0.1"
+  version = "0.0.2"
 
   stack_name  = "${var.stack_name}"
   aws_profile = "${var.aws_profile}"
