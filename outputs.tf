8eb2c0fe68ddb126691db4e7b3d6a8985d3e8720
Lionel Martin <corsaaaire@gmail.com>
2018-06-05 12:19:48 +0100

    fixing outputs

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,10 @@ output "s3_bucket_website" {
   value = "${module.s3.s3_bucket_website}"
 }
 
+output "s3_bucket" {
+  value = "${module.s3.s3_bucket}"
+}
+
 output "cloudfront_domain_name" {
   value = "${module.cdn.cloudfront_domain_name}"
 }
